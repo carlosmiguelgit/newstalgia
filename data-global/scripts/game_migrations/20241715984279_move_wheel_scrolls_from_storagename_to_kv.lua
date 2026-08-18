d9d1c0444fd67b2116b57a840b29ec157086b343
carlosmiguelgit <carlosmiguelxiaomi@gmail.com>
2026-08-18 08:05:23 -0300

    a
    a

--- data-global/scripts/game_migrations/20241715984279_move_wheel_scrolls_from_storagename_to_kv.lua
+++ data-global/scripts/game_migrations/20241715984279_move_wheel_scrolls_from_storagename_to_kv.lua
@@ -8,9 +8,11 @@ local promotionScrolls = {
 
 local function migrate(player)
 	for _, scrollTable in ipairs(promotionScrolls) do
-		local oldStorage = player:getStorageValueByName(scrollTable.oldScroll)
-		if oldStorage > 0 then
-			player:kv():scoped("wheel-of-destiny"):scoped("scrolls"):set(scrollTable.newScroll, true)
+		if not player:kv():scoped("wheel-of-destiny"):scoped("scrolls"):get(scrollTable.newScroll) then
+			local oldStorage = player:getStorageValueByName(scrollTable.oldScroll)
+			if oldStorage > 0 then
+				player:kv():scoped("wheel-of-destiny"):scoped("scrolls"):set(scrollTable.newScroll, true)
+			end
 		end
 	end
 end
